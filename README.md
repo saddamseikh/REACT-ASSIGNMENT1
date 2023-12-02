f32dcc4d464d145a0690e977fa1e1d1b0a0d242c
saddam seikh <saddamseikh2229@gmail.com>
2023-12-02 11:57:51 +0530

    Create a component named "Button" that takes two props - "text" and "onClick

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # ReactJs Problem statement
 
 ### 1. Create a component named "Person" that takes two props - "name" and "age". The component should display the person's name and age in a paragraph element.
+
+### 2. Create a component named "Button" that takes two props - "text" and "onClick". The component should display a button element with the given text and call the onClick function when clicked.
--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
+import Button from "./Button";
 import Person from "./Person";
 
 function App() {
   return (
-    <div className="App" style={{ fontSize: "32px" }}>
-      <Person name="Saddam sk" age="23" />
-      <Person name="Abdul Rahaman " age="22" />
-    </div>
+    <>
+      <div className="App" style={{ fontSize: "32px" }}>
+        <Person name="Saddam sk" age="23" />
+        <Person name="Abdul Rahaman " age="22" />
+      </div>
+      <Button text="Click me!" onClick={() => console.log("Clicked me ")} />
+    </>
   );
 }
 
